1900ab658814a92491c7d0dbd1ddd9337e7efece
JenilGajjar20 <68738624+JenilGajjar20@users.noreply.github.com>
2021-04-20 10:10:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+
+setInterval(function() {
+    let day = new Date();
+
+    let hours = day.getHours() * 30;
+    let minutes = day.getMinutes() * deg;
+    let seconds = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${(hours) + (minutes/12)}deg)`;
+    mn.style.transform = `rotateZ(${minutes}deg)`;
+    sc.style.transform = `rotateZ(${seconds}deg)`;
+});
